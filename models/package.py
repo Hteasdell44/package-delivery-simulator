77fb91e407d0085a20228a2945055bb4a1f56ca6
hteasdell44 <hteasdell44@gmail.com>
2024-03-16 05:46:33 -0400

    Added line to prevent packages leaving before 8am

--- models/package.py
+++ models/package.py
@@ -20,8 +20,13 @@ class Package:
 
     # Update the status of the package based on the provided time
     def update_status(self, convert_timedelta):
+
+        # Prevents any truck/package from leaving the station before 8am.
+        if convert_timedelta < datetime.timedelta(hours=8):
+            self.status = "At The Hub!"
+            
         # Prevents trucks two and three from leaving before their designated times.
-        if self.ID in [2, 4, 5, 7, 8, 9, 10, 11, 12, 25, 28, 32, 33] and convert_timedelta < datetime.timedelta(hours=10, minutes=20) or self.ID in [3, 6, 17, 18, 19, 21, 22, 23, 24, 26, 27, 35, 36, 38, 39] and convert_timedelta < datetime.timedelta(hours=9, minutes=5):
+        if self.ID in [2, 4, 5, 7, 8, 9, 10, 11, 12, 25, 28, 32, 33] and convert_timedelta < datetime.timedelta(hours=10, minutes=20) or self.ID in [3, 6, 17, 18, 21, 22, 23, 24, 26, 27, 35, 36, 38, 39] and convert_timedelta < datetime.timedelta(hours=9, minutes=5):
             self.status = "At The Hub!"
 
         # Prevents any truck from leaving before 8, if after 8 and elgible to leave the package is marked en route.
